7d01c72b74469ce7e1eebc266ed786eb7147a4d5
amin <amin@MI6>
2017-11-10 02:06:57 -0400

    Created Xml Adapter for LocalDate class.

---
+++ src/core/entities/adapters/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * This package contains adapters for marshalling and unmarshalling objects.
+ * 
+ * @author Francisco Frias
+ * @author Abel Guzman
+ * @author Amin Guzman
+ */
+package core.entities.adapters;
